4e201955888149cff9ae0c3919a7a1b665ab48c8
pierresamaritano <pierre.samaritano@icloud.com>
2026-09-02 11:19:07 +0200

    route fonction stripe

---
+++ app/api/stripe/capture/route.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+import Stripe from "stripe";
+
+export async function POST(req: Request) {
+  try {
+    const { paymentIntentId } = await req.json();
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "", { apiVersion: "2025-02-24.acacia" });
+    
+    // On encaisse l'empreinte bancaire !
+    const intent = await stripe.paymentIntents.capture(paymentIntentId);
+    return NextResponse.json({ success: true, intent });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
